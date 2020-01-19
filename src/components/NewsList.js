242e8f064e5b345c888c79284cfcaba09ff3ed8d
eunhyeKim <keh7264@gmail.com>
2020-01-19 21:05:21 +0900

    Add Categories

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -16,7 +16,7 @@ export const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -24,8 +24,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === 'all' ? '' : `&category=${category}`;
         const respose = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=8c602f099926471dadbe392a5a228844',
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=8c602f099926471dadbe392a5a228844`,
         );
         setArticles(respose.data.articles);
       } catch (e) {
@@ -34,7 +35,8 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
+
   if (loading) {
     return <NewsListBlock>대기 중...</NewsListBlock>;
   }
