69a5bf21c495d753c354c2d207b129a0d1fbf9a5
eunhyeKim <keh7264@gmail.com>
2020-01-19 20:40:52 +0900

    Create NewsList and NewsItem

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-
+import NewsList from './components/NewsList';
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=8c602f099926471dadbe392a5a228844',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 }
 
 export default App;
